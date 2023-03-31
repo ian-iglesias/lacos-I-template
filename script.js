8f3ca1554964944b78a35352c4e3151f15d80d9d
ian-iglesias <ianiglesias6798@gmail.com>
2023-03-31 13:11:59 -0300

    resolve

--- script.js
+++ script.js
@@ -1,7 +1,29 @@
-const numero = prompt("Digite um número");
+// let numero = +prompt("Digite um número");
 
-let resultadoSoma = 0
+// let resultadoSoma = 0
 
-while(numero !== 0){
-    resultadoSoma = resultadoSoma + numero
-}
+// while(numero !== 0){
+//     resultadoSoma = resultadoSoma + numero
+//     numero = +prompt("Digite um número")
+// };
+
+// console.log(resultadoSoma);
+
+//-------------------------------------------------
+
+// const numero = +prompt("Digite um número");
+
+// for(let i = 0; i <= numero; i++){
+//     console.log(i)
+// };
+
+//-------------------------------------------------
+
+const array = [5, 3, 8, 5, 1, 6, 9];
+//             0  1  2  3  4  5  6
+
+for(let i = 0; i < array.length; i++ ){
+    console.log(`O número do índice ${i} é ${array[i]}`)
+};
+
+console.log("Done!")
